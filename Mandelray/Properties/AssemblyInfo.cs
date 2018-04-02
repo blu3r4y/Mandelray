d59e97f32b3bf07ee606c7be65366905d5c24f15
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-04-02 16:42:54 +0200

    Update AssemblyInfo for 1.0.0 release

--- Mandelray/Properties/AssemblyInfo.cs
+++ Mandelray/Properties/AssemblyInfo.cs
@@ -1,15 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("1.0.0")]
+
 [assembly: AssemblyTitle("Mandelray")]
 [assembly: AssemblyDescription("A Mandelbrot Set Viewer in C#")]
-[assembly: AssemblyConfiguration("")]
+
 [assembly: AssemblyCompany("Mario Kahlhofer")]
 [assembly: AssemblyProduct("Mandelray")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright("Copyright © 2018 Mario Kahlhofer - blu3r4y")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: Guid("8f9e7ae8-ae4c-495d-ac51-c5106fcbbd0e")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
